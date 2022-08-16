f16a840d1f2ad55f4daf6b852cbc885c6d1c6901
pschaefferkoetter <prschaefferkoetter@gmail.com>
2022-08-16 09:30:54 -0600

    feat: add file front-end file structure

--- client/src/shared/__tests__/sum.test.js
+++
@@ -1,9 +0,0 @@
-const sum = require("../sum");
-
-describe("test sum function", () => {
-  it("should sum to the expected value", () => {
-    const a = 2;
-    const b = 3;
-    expect(sum(3, 2)).toEqual(a + b);
-  });
-});
